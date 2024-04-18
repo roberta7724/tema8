2fa2d47c92286a8d7822d9adb89c4880337480a4
roberta7724 <roberta.bara@emanuel.ro>
2024-04-18 11:26:30 +0300

    tema

---
+++ script.js
@@ -0,0 +1,233 @@
+function setup() {
+    createCanvas(570,360);
+}
+function draw() {
+    background(143,188,143);
+    dsquare();
+}
+
+function dsquare() {
+fill("red");
+square(50,50,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("1", 50 + 25,50 + 25);
+
+
+fill(139,0,139);
+square(120,50,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("2", 120 + 25,50 + 25);
+
+
+fill(154,205,50);
+square(190,50,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("3", 190 + 25,50 + 25);
+
+
+fill(0,191,255);
+square(260,50,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("4", 260 + 25,50 + 25);
+
+
+fill(255,20,147);
+square(330,50,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("5", 330 + 25,50 + 25);
+
+
+fill(173,255,47);
+square(400,50,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("6", 400 + 25,50 + 25);
+
+
+fill(255,127,80);
+square(470,50,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("7", 470 + 25,50 + 25);
+
+
+fill(32,178,170);
+square(50,120,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("8", 50 + 25,120 + 25);
+
+
+fill(128,128,0);
+square(120,120,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("9", 120 + 25,120 + 25);
+
+
+fill(255,0,255);
+square(190,120,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("10", 190 + 25,120 + 25);
+
+
+fill(138,43,226);
+square(260,120,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("11", 260 + 25,120 + 25);
+
+
+fill(169,169,169);
+square(330,120,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("12", 330 + 25,120 + 25);
+
+
+fill(224,255,255);
+square(400,120,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("13", 400 + 25,120 + 25);
+
+
+fill(240,230,140);
+square(470,120,50);
+fill("black");
+textSize(25);
+textAlign(CENTER, CENTER);
+text("14", 470 + 25,120 + 25);
+
+
+fill(255,228,225);
+square(50,190,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("15", 50 + 25,190 + 25);
+
+
+fill(32,178,170);
+square(120,190,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("16", 120 + 25,190 + 25);
+
+
+fill(128,0,0);
+square(190,190,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("17", 190 + 25,190 + 25);
+
+
+fill(250,240,230);
+square(260,190,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("18", 260 + 25,190 + 25);
+
+
+fill(102,205,170);
+square(330,190,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("19", 330 + 25,190 + 25);
+
+
+fill(147,112,219);
+square(400,190,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("20", 400 + 25,190 + 25);
+
+
+fill(199,21,133);
+square(470,190,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("21", 470 + 25,190 + 25);
+
+
+fill(1255,69,0);
+square(50,260,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("22", 50 + 25,260 + 25);
+
+
+fill(218,112,214);
+square(120,260,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("23", 120 + 25,260 + 25);
+
+
+fill(152,251,152);
+square(190,260,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("24", 190 + 25,260 + 25);
+
+
+fill(175,238,238);
+square(260,260,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("25", 260 + 25,260 + 25);
+
+
+fill(219,112,147);
+square(330,260,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("26", 330 + 25,260 + 25);
+
+
+
+fill(255,218,185);
+square(400,260,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("27", 400 + 25,260 + 25);
+
+
+fill(102,51,153);
+square(470,260,50);
+fill("black");
+textSize(25); 
+textAlign(CENTER, CENTER);
+text("28", 470 + 25,260 + 25);
+}
